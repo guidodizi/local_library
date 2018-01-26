8b212a0bc8746b4eeabdbcff9f67955515dbd157
Guido Dizioli <guidodizi@hotmail.com>
2018-01-26 18:10:56 -0300

    inital commit 2 D:

---
+++ models/author.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+var moment = require('moment'); // For date handling.
+
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema(
+    {
+    first_name: {type: String, required: true, max: 100},
+    family_name: {type: String, required: true, max: 100},
+    date_of_birth: { type: Date },
+    date_of_death: { type: Date },
+    }
+  );
+
+// Virtual for author "full" name.
+AuthorSchema.virtual('name').get(function () {  return this.family_name +', '+ this.first_name;});
+
+// Virtual for this author instance URL.
+AuthorSchema.virtual('url').get(function () {  return '/catalog/author/'+ this._id});
+
+AuthorSchema.virtual('date_of_birth_formatted').get(function () {  return moment(this.date_of_birth).format('MMMM Do, YYYY');});
+
+AuthorSchema.virtual('date_of_death_formatted').get(function () {  return moment(this.date_of_death).format('MMMM Do, YYYY');});
+
+// Export model.
+module.exports = mongoose.model('Author', AuthorSchema);
