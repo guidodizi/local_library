5a430b395d8895a2b073e47a3cf2e1b7e3130670
Guido Dizioli <guidodizi@hotmail.com>
2018-01-30 12:38:48 -0300

    fix visual properties

--- models/author.js
+++ models/author.js
@@ -17,10 +17,15 @@ AuthorSchema.virtual('name').get(function () {  return this.family_name +', '+ t
 
 // Virtual for this author instance URL.
 AuthorSchema.virtual('url').get(function () {  return '/catalog/author/'+ this._id});
-
-AuthorSchema.virtual('date_of_birth_formatted').get(function () {  return moment(this.date_of_birth).format('MMMM Do, YYYY');});
-
-AuthorSchema.virtual('date_of_death_formatted').get(function () {  return moment(this.date_of_death).format('MMMM Do, YYYY');});
+AuthorSchema.virtual('date_of_birth_formatted').get(function () {  
+  return this.date_of_birth ? moment(this.date_of_birth).format('MMMM Do, YYYY') : '';
+});
+AuthorSchema.virtual('date_of_death_formatted').get(function () {  
+  return this.date_of_death ? moment(this.date_of_death).format('MMMM Do, YYYY') : '';
+});
+AuthorSchema.virtual('lifespan').get(function () {  
+  return this.date_of_birth_formatted + ' - ' + this.date_of_death_formatted;
+});
 
 // Export model.
 module.exports = mongoose.model('Author', AuthorSchema);
