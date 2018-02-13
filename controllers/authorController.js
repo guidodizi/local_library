e7586f5adcc30f2485b6a97899131c1ea9f4478a
Guido Dizioli <guidodizi@hotmail.com>
2018-02-13 17:30:09 -0300

    author create functionality

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,7 +1,9 @@
-var Author = require('../models/author')
-var Book = require('../models/book')
+const Author = require('../models/author')
+const Book = require('../models/book')
 
-var async = require('async')
+const async = require('async')
+const { body, validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
 
 // Display list of all Authors.
 exports.author_list = function (req, res, next) {
@@ -42,13 +44,52 @@ exports.author_detail = function (req, res, next) {
 
 // Display Author create form on GET.
 exports.author_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create GET');
+    res.render('author_form', { title: 'Create Author' })
 };
 
 // Handle Author create on POST.
-exports.author_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create POST');
-};
+exports.author_create_post = [
+    body('first_name').isLength({min: 1}).trim().withMessage('First name must be specified')
+    .isAlpha().withMessage('First name should only contain letters '),
+
+    body('family_name').isLength({min: 1}).trim().withMessage('Family name must be specified')
+    .isAlpha().withMessage('Family name should only contain letters '),
+
+    body('date_of_birth', 'Invalid date of birth').optional({ checkFalsy: true}).isISO8601(),
+    
+    body('date_of_death', 'Invalid date of death').optional({ checkFalsy: true}).isISO8601(),
+
+    sanitizeBody('first_name').trim().escape(),
+    
+    sanitizeBody('family_name').trim().escape(),
+    
+    sanitizeBody('date_of_birth').toDate(),
+    
+    sanitizeBody('date_of_death').toDate(),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+
+        var author = new Author({
+            first_name: req.body.first_name,
+            family_name: req.body.family_name,
+            date_of_birth: req.body.date_of_birth,
+            date_of_death: req.body.date_of_death,
+        });
+
+        if (!errors.isEmpty()){
+            res.render('author_form', {title: 'Create Author', author: author, errors: errors.array()});
+            return;
+        }
+        else{
+            author.save(function(err){
+                if (err) {return next(err); }
+                res.redirect(author.url);
+            })
+        }
+    }
+    
+]
 
 // Display Author delete form on GET.
 exports.author_delete_get = function(req, res) {
